851d797f123c8c460d1cf5bed575ca75c453c00b
Adam Cross <adam.cross@mademedia.co.uk>
2012-10-23 13:25:21 +0100

    created inheritance program

---
+++ Inheritance.rb
@@ -0,0 +1,32 @@
+class Main
+  def self.sayHello(people)
+    for person in people
+      p "Hello #{person.name}, you are #{person.age}, and #{person.gender}"
+    end
+  end
+end
+
+class Person
+  attr_reader :name, :age, :gender
+  def initialize(name, age, gender)
+    @name=name
+    @age = age
+    @gender = gender
+  end
+  
+end
+
+class Man < Person
+  def initialize(name, age)
+    super(name, age, "Male")
+  end
+end  
+
+class Woman < Person
+  def initialize(name, age)
+    super(name, age, "Female")
+  end
+end
+
+people = [Man.new("Adam", 21), Woman.new("Eve", 20)]
+Main.sayHello(people)
